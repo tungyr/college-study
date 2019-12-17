ceea7c92d513617e86ed267b07c863c3b636864a
tungyr <rotkivem@gmail.com>
2019-12-17 20:15:20 +0300

    task_2 falling ball while development

--- task_2_falling_ball/task_2_falling_ball/calc.cpp
+++ task_2_falling_ball/task_2_falling_ball/calc.cpp
@@ -6,16 +6,21 @@ int height_calc(double height)
 	int second = 0;
 	double height_rest = 1;
 
-	std::cout << "At "<< second << "seconds, the ball is at height "<< height << "meters." << std::endl;
+	std::cout << "At "<< second << " seconds, the ball is at height "<< height << " meters." << std::endl;
 
-	while (height_rest > 0)
+	//if (height == 0)
+		//std::cout << "wrong height!";
+	//while (height - (9.8 * (second + 1) * (second + 1) / 2) > 0 && height != 0)
+
+	while (height > 0)
 	{
 		second = second + 1;
 		ball_fall = 9.8 * second * second / 2;
 		height_rest = height - ball_fall;
-		std::cout << "At " << second << "seconds, the ball is at height " << height_rest << "meters." << std::endl;
+		std::cout << "At " << second << " seconds, the ball is at height " << height_rest << " meters." << std::endl;
+		height = (height - (9.8 * (second + 1) * (second + 1) / 2));
 	}
-	std::cout << "At " << second << "seconds, the ball is on the ground.";
+	std::cout << "At " << second << " seconds, the ball is on the ground.";
 
 	return 0;
 		
