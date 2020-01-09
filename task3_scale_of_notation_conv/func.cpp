c4b21086b349dcc04e2f5911a3632f9772d2a900
tungyr <rotkivem@gmail.com>
2020-01-09 15:35:02 +0300

    task 3 binary system conv

---
+++ task3_scale_of_notation_conv/func.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "func.h"
+
+
+
+int convert(int number)
+{
+	int max = 256;
+	int summ = 0;
+	char bin = ' ';
+
+	while (max != 1)
+	{
+		if (number < max)
+			max = max / 2;
+		else
+			if (summ  
+			summ = summ + max;
+			bin = bin + '1';
+	}
+	
+
+
+}
---
+++ task3_scale_of_notation_conv/main.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include "func.h"
+
+int main()
+{
+    std::cout << "Enter an integer: ";
+	int number;
+	std::cin >> number;
+	convert(number);
+}
+
