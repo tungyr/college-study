078477aa5b1bcf992ec506f11d171b1e45253ed4
tungyr <rotkivem@gmail.com>
2020-01-25 21:02:40 +0300

    tried to improve third task of scale of notation

--- task3_scale_of_notation_conv/func.cpp
+++ task3_scale_of_notation_conv/func.cpp
@@ -17,26 +17,29 @@ string convert(int number)
 	{
 		reminder = number % 2;
 		std::string bin_new = std::to_string(reminder);
-		
-
 		bin = bin + bin_new;
 		number = number / 2;
 		//std::cout << bin_new;
 		zero_loop = 0;
 		counter++;
 
-		if (counter == 4)
-		{
-			bin = bin + " ";
-		}
-		
+
 	}
 
 	int len = bin.length();
 	int n = len - 1;
 	for (int i = 0; i < (len / 2); i++)
-		swap(bin[i], bin[n]);
+		std::swap(bin[i], bin[n]);
 		n = n - 1;
+
+	//std::string bin_final = "";
+	//for (int j = 0; j < (len + 1); j++)
+	//	if (j == 4){
+	//		bin_final = bin_final + " ";
+	//	}
+	//	else{
+	//		bin_final = bin_final + bin[j];
+	//	}
 
 	//std::cout << bin;
 
